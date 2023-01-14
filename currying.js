136a2d869955f2ca1fd0af305502c22ff6730e8b
Mauricio Torres <otarksk@gmail.com>
2023-01-14 20:42:27 -0300

    currying add second part

--- currying.js
+++ currying.js
@@ -14,3 +14,8 @@ function curry(fn) {
 
 const curriedSum = curry(sum);
 console.log(curriedSum(2)(3)(5));
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+const add5 = add3(5)
+console.log(add5);
