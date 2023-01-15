8633df112a43c368cb69dbb41b8865737b1437a8
Mauricio Torres <otarksk@gmail.com>
2023-01-14 21:02:18 -0300

    new bending

--- this.js
+++ this.js
@@ -6,3 +6,21 @@ const person = {
 }
 //* Implicit binding 
 person.sayMyName()
+
+//* Explicit binding 
+function saymyName(){
+    console.log(`My name is ${this.name}`);
+}
+
+saymyName.call(person)
+
+//* New binding 
+function Person(name){
+    this.name = name
+}
+
+const p1 = new Person('Vishwas')
+const p2 = new Person('Batman')
+
+console.log(p1.name, p2.name);
+//* Default binding 
